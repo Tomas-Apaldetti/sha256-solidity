950c3b6c79adc35e7eb01fbd853691e4365bafd3
Tomás Apaldetti <tapaldetti@netrixllc.com>
2024-02-10 13:16:00 -0300

    sha256 working

--- src/Sha256.sol
+++ src/Sha256.sol
@@ -19,19 +19,16 @@ function shift_right_w32(bytes4 x, uint8 n) pure returns (bytes4) {
 /**
  * Reinterpret a 256 bit number as a 32bit-word
  */
-function uint32_to_bytes4(uint256 x) pure returns (bytes4 b) {
-    b = bytes4(0x0);
-    assembly {
-        mstore(add(b, 4), x)
-    }
+function uint32_to_bytes4(uint32 x) pure returns (bytes4) {
+    return bytes4(x);
 }
 
 /**
- * Sum multiple 32bit-words as if they were number in mod 2^256, then reinterpret the result as a new word
+ * Sum multiple 32bit-words as if they were number in mod 2^32, then reinterpret the result as a new word
  */
 function sum_w32(bytes4[2] memory words) pure returns (bytes4) {
     uint32 acc = 0;
-    for (uint32 i = 0; i < words.length; i++) {
+    for (uint32 i = 0; i < 2; i++) {
         unchecked {
             acc += uint32(words[i]);
         }
@@ -41,11 +38,11 @@ function sum_w32(bytes4[2] memory words) pure returns (bytes4) {
 }
 
 /**
- * Sum multiple 32bit-words as if they were number in mod 2^256, then reinterpret the result as a new word
+ * Sum multiple 32bit-words as if they were number in mod 2^32, then reinterpret the result as a new word
  */
 function sum_w32(bytes4[3] memory words) pure returns (bytes4) {
     uint32 acc = 0;
-    for (uint32 i = 0; i < words.length; i++) {
+    for (uint32 i = 0; i < 3; i++) {
         unchecked {
             acc += uint32(words[i]);
         }
@@ -55,21 +52,20 @@ function sum_w32(bytes4[3] memory words) pure returns (bytes4) {
 }
 
 /**
- * Sum multiple 32bit-words as if they were number in mod 2^256, then reinterpret the result as a new word
+ * Sum multiple 32bit-words as if they were number in mod 2^32, then reinterpret the result as a new word
  */
 function sum_w32(bytes4[4] memory words) pure returns (bytes4) {
     uint32 acc = 0;
-    for (uint32 i = 0; i < words.length; i++) {
+    for (uint32 i = 0; i < 4; i++) {
         unchecked {
             acc += uint32(words[i]);
         }
     }
-
     return uint32_to_bytes4(acc);
 }
 
 /**
- * Sum multiple 32bit-words as if they were number in mod 2^256, then reinterpret the result as a new word
+ * Sum multiple 32bit-words as if they were number in mod 2^32, then reinterpret the result as a new word
  */
 function sum_w32(bytes4[5] memory words) pure returns (bytes4) {
     uint32 acc = 0;
@@ -90,7 +86,6 @@ function mix(bytes4[] memory w, uint32 t) pure returns (bytes4) {
     bytes4 s0 = rotate_right_w32(w[t - 15], 7) ^
         rotate_right_w32(w[t - 15], 18) ^
         shift_right_w32(w[t - 15], 3);
-
     bytes4 s1 = rotate_right_w32(w[t - 2], 17) ^
         rotate_right_w32(w[t - 2], 19) ^
         shift_right_w32(w[t - 2], 10);
@@ -122,7 +117,7 @@ function _sha_round(bytes4[] memory w, PartialState memory p, bytes4[64] memory 
             rotate_right_w32(p.a, 2) ^ rotate_right_w32(p.a, 13) ^ rotate_right_w32(p.a, 22), 
             maj(p.a, p.b, p.c)
         ]);
-
+        
         p.h = p.g;
         p.g = p.f;
         p.f = p.e;
@@ -211,32 +206,30 @@ function sha_round(
         bytes4(0xbef9a3f7),
         bytes4(0xc67178f2)
     ];
-
     PartialState memory p = _sha_round(
         w, 
         PartialState({
-            a: current_state[0],
-            b: current_state[1],
-            c: current_state[2],
-            d: current_state[3],
-            e: current_state[4],
-            f: current_state[5],
-            g: current_state[6],
-            h: current_state[7]
+            a: bytes4(current_state),
+            b: bytes4(current_state << 32),
+            c: bytes4(current_state << 64),
+            d: bytes4(current_state << 96),
+            e: bytes4(current_state << 128),
+            f: bytes4(current_state << 160),
+            g: bytes4(current_state << 192),
+            h: bytes4(current_state << 224)
         }),
         constants
     );
-
     bytes32 result;
 
-    result |= bytes32(p.a) >> 0;
-    result |= bytes32(p.b) >> 32;
-    result |= bytes32(p.c) >> 64;
-    result |= bytes32(p.d) >> 96;
-    result |= bytes32(p.e) >> 128;
-    result |= bytes32(p.f) >> 160;
-    result |= bytes32(p.g) >> 192;
-    result |= bytes32(p.h) >> 224;
+    result |= bytes32(sum_w32([bytes4(current_state), p.a])) >> 0;
+    result |= bytes32(sum_w32([bytes4(current_state << 32), p.b])) >> 32;
+    result |= bytes32(sum_w32([bytes4(current_state << 64), p.c])) >> 64;
+    result |= bytes32(sum_w32([bytes4(current_state << 96), p.d])) >> 96;
+    result |= bytes32(sum_w32([bytes4(current_state << 128), p.e])) >> 128;
+    result |= bytes32(sum_w32([bytes4(current_state << 160), p.f])) >> 160;
+    result |= bytes32(sum_w32([bytes4(current_state << 192), p.g])) >> 192;
+    result |= bytes32(sum_w32([bytes4(current_state << 224), p.h])) >> 224;
 
     return result;
 }
@@ -247,11 +240,11 @@ function sha_round(
 function chunk_to_schedule(bytes memory chunk) pure returns (bytes4[] memory) {
     require(chunk.length == 64);
     bytes4[] memory schedule = new bytes4[](64);
-    for (uint i = 0; i < chunk.length; i += 4) {
-        schedule[i] |= bytes4(chunk[i]) >> 0;
-        schedule[i] |= bytes4(chunk[i + 1]) >> 8;
-        schedule[i] |= bytes4(chunk[i + 2]) >> 16;
-        schedule[i] |= bytes4(chunk[i + 3]) >> 24;
+    for (uint i = 0; i < 16; i++) {
+        schedule[i] |= bytes4(chunk[i * 4]) >> 0;
+        schedule[i] |= bytes4(chunk[i * 4 + 1]) >> 8;
+        schedule[i] |= bytes4(chunk[i * 4 + 2]) >> 16;
+        schedule[i] |= bytes4(chunk[i * 4 + 3]) >> 24;
     }
     return schedule;
 }
@@ -265,7 +258,7 @@ function slice(
     bytes memory copy = new bytes(end - start);
 
     for (uint i = start; i < end; i++) {
-        copy[i] = input[i];
+        copy[i - start] = input[i];
     }
 
     return copy;
@@ -284,11 +277,10 @@ function slice_then_append_bit_1(
     uint chunk_length = chunk_end - chunk_start;
 
     for (uint i = chunk_start; i < chunk_end; i++) {
-        copy[i] = input[i];
+        copy[i - chunk_start] = input[i];
     }
 
-    copy[chunk_length + 1] = bytes1(0x80);
-
+    copy[chunk_length] = bytes1(0x80);
     return copy;
 }
 
@@ -297,13 +289,12 @@ function slice_then_append_bit_1(
  */
 function insert_len(
     bytes memory chunk,
-    uint256 total_len
+    uint64 total_len
 ) pure returns (bytes memory) {
-    bytes32 byte_len = bytes32(total_len << 3);
+    bytes8 byte_len = bytes8(total_len * 8);
     for (uint i = 56; i < 64; i++) {
-        chunk[i] = byte_len[i];
+        chunk[i] = byte_len[i - 56];
     }
-
     return chunk;
 }
 
@@ -317,11 +308,9 @@ function do_64bytes_chunk(
 ) pure returns (bytes32) {
     require(chunk.length == 64);
     bytes4[] memory schedule = chunk_to_schedule(chunk);
-
     for (uint32 t = 16; t < 64; t++) {
         schedule[t] = mix(schedule, t);
     }
-
     return sha_round(schedule, curr_hash);
 }
 
@@ -333,7 +322,7 @@ function do_chunk(
     uint chunk_start,
     uint chunk_end,
     bytes32 curr_hash,
-    uint total_len
+    uint64 total_len
 ) pure returns (bytes32) {
     require(input.length == total_len && input.length >= chunk_end && chunk_end > chunk_start);
     uint chunk_length = chunk_end - chunk_start;
@@ -374,29 +363,20 @@ function do_chunk(
 }
 
 function tp_sha256(bytes memory input) pure returns (bytes32) {
+    require(input.length < 2**64);
+    uint64 total_len = uint64(input.length);
     bytes32 curr_hash = 0x6a09e667bb67ae853c6ef372a54ff53a510e527f9b05688c1f83d9ab5be0cd19;
+
     if(input.length == 0){
         bytes memory empty_chunk = new bytes(64);
         empty_chunk[0] = 0x80;
         curr_hash = do_64bytes_chunk(empty_chunk, curr_hash);
     }
+
     for (uint i = 0; i < input.length; i += 64) {
         uint offset = i + 64 > input.length ? input.length : i + 64;
-        curr_hash = do_chunk(input, i, offset, curr_hash, input.length);
+        curr_hash = do_chunk(input, i, offset, curr_hash, total_len);
     }
 
     return curr_hash;
-}
-
-function hex_digest(bytes memory _bytes) pure returns (string memory){
-    bytes memory alphabet = "0123456789abcdef";
-    bytes memory str = new bytes(_bytes.length * 2);
-
-    for (uint256 i = 0; i < _bytes.length; i++) {
-        str[i * 2] = alphabet[uint8((_bytes[i] >> 4) & 0x0F)];
-        str[i * 2 + 1] = alphabet[uint8(_bytes[i] & 0x0F)];
-    }
-
-    return string(str);
-    
 }
--- test/Sha256.t.sol
+++ test/Sha256.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
-import {tp_sha256, hex_digest} from "../src/Sha256.sol";
+import {console} from "forge-std/console.sol";
+import {Test} from "forge-std/Test.sol";
+import {tp_sha256} from "../src/Sha256.sol";
 
 contract Sha256Test is Test {
 
@@ -12,4 +13,42 @@ contract Sha256Test is Test {
         assertEq(mine, language);
     }
 
+    function test_first() public {
+        bytes32 mine = tp_sha256("This is a test");
+        bytes32 language = sha256("This is a test");
+        assertEq(mine, language);
+    }
+
+    function test_second() public {
+        bytes32 mine = tp_sha256("Lorem ipsum dolor sit amet, consectetur adipiscing elit integer.");
+        bytes32 language = sha256("Lorem ipsum dolor sit amet, consectetur adipiscing elit integer.");
+        assertEq(mine, language);
+    }
+
+    function test_third() public {
+        bytes32 mine = tp_sha256("Lorem ipsum dolor sit amet, consectetur adipiscing tellus.");
+        bytes32 language = sha256("Lorem ipsum dolor sit amet, consectetur adipiscing tellus.");
+        assertEq(mine, language);
+    }
+
+    function test_fourth() public {
+        bytes memory str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit volutpat.";
+        bytes32 mine = tp_sha256(str);
+        bytes32 language = sha256(str);
+        assertEq(mine, language);
+    }
+
+    function test_fifth() public {
+        bytes memory str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit dui non augue facilisis rhoncus. Ut egestas.";
+        bytes32 mine = tp_sha256(str);
+        bytes32 language = sha256(str);
+        assertEq(mine, language);
+    }
+
+    function test_sixth() public {
+        bytes memory str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eleifend ut turpis a fringilla. Mauris mi ligula, volutpat quam.";
+        bytes32 mine = tp_sha256(str);
+        bytes32 language = sha256(str);
+        assertEq(mine, language);
+    }
 }
